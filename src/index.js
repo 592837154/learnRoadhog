21139a1e0605d3542badd92304dbe8c5c9d22b5d
zhukai <zhukai03@kuaishou.com>
2020-01-06 21:17:35 +0800

    a

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './index.css'
+ReactDOM.render(
+    <h1 className={styles.aaa}>
+        hello
+    </h1>,
+    document.getElementById('root')
+)
